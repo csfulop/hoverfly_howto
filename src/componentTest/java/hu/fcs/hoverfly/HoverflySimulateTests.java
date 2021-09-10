a4ba5411eb4f28caccdf4e8909691f0b1489b992
Csaba Fülöp <csfulop@users.noreply.github.com>
2021-09-10 14:07:58 +0200

    add test with Hoverfly simulate

--- src/componentTest/java/hu/fcs/hoverfly/HoverflySimulateTests.java
+++ src/componentTest/java/hu/fcs/hoverfly/HoverflySimulateTests.java
@@ -1,18 +1,14 @@
 package hu.fcs.hoverfly;
 
 import io.specto.hoverfly.junit5.HoverflyExtension;
-import io.specto.hoverfly.junit5.api.HoverflyCapture;
 import io.specto.hoverfly.junit5.api.HoverflyConfig;
+import io.specto.hoverfly.junit5.api.HoverflySimulate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,33 +19,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
-@Testcontainers
 @ExtendWith(HoverflyExtension.class)
-@HoverflyCapture(
+@TestPropertySource(properties = {"api.port = 3333"})
+@HoverflySimulate(
+    enableAutoCapture = false,
     config = @HoverflyConfig(
         plainHttpTunneling = true
     ),
-    // path needs to be overridden because by default it saves the captures into the test folder
-    path = "src/componentTest/resources/hoverfly"
+    source = @HoverflySimulate.Source(
+        // by default it tries to put/get capture files from test folder
+        // https://docs.hoverfly.io/projects/hoverfly-java/en/latest/pages/junit5/extension.html#simulate
+        type = HoverflySimulate.SourceType.FILE,
+        value = "src/componentTest/resources/hoverfly/hu_fcs_hoverfly_HoverflySimulateTests.json"
+        // NOTE: capture file captured by HoverflyCaptureTests
+        // needs to be renamed to match the name of THIS test class
+        // and the port needs to be updated because Testcontainers by design uses random port mapping
+        )
 )
-class HoverflyCaptureTests {
-
-    @Container
-    private static final GenericContainer<?> container =
-        new GenericContainer<>("dotronglong/faker:stable")
-            .withExposedPorts(3030)
-            .withFileSystemBind("src/componentTest/resources/mocks", "/app/mocks");
-
-    @DynamicPropertySource
-    private static void registerApiPort(DynamicPropertyRegistry registry) {
-        registry.add("api.port", () -> container.getMappedPort(3030));
-    }
+class HoverflySimulateTests {
 
     @LocalServerPort
     private int port;
 
     @Test
-    void testGetWithFakeApiAndCaptureWithHoverfly() {
+    void testGetWithHoverflySimulate() {
         // given
         WebTestClient webTestClient = WebTestClient
             .bindToServer()
@@ -66,6 +59,6 @@ class HoverflyCaptureTests {
             .getResponseBody()
             .toStream()
             .collect(Collectors.toList());
-        assertThat(users).hasSize(2);
+        assertThat(users).hasSize(3);
     }
 }
